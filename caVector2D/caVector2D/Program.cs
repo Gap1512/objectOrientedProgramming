ca2ad189467334b1b589456016ae90b205882e37
Gustavo Alves Pacheco <42452998+Gap1512@users.noreply.github.com>
2019-03-29 16:17:06 -0300

    Laboratório 1

---
+++ caVector2D/caVector2D/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caVector2D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vetor2D a1 = new Vetor2D();
+            Vetor2D a2 = new Vetor2D(1, 1);
+            Vetor2D i = new Vetor2D(1, 0);
+            Vetor2D j = new Vetor2D(0, 1);
+
+            Console.WriteLine("Valores Iniciais: ");
+            a1.print();
+            a2.print();
+
+            a1.setX(10);
+            a2.setY(5);
+
+            Console.WriteLine("Valores Atualizados: ");
+            a1.print();
+            a2.print();
+
+            Console.WriteLine("Produtos Escalares: ");
+            Console.WriteLine(a1.escalar(a2));
+            Console.WriteLine(a2.escalar(a1));
+            Console.WriteLine(i.escalar(j));
+
+            Console.WriteLine("Modulos: ");
+            Console.WriteLine(a1.modulo());
+            Console.WriteLine(a2.modulo());
+            Console.WriteLine("|i|: " + i.modulo());
+            Console.WriteLine("|j|: " + j.modulo());
+
+            Console.WriteLine("Angulos: ");
+            Console.WriteLine(a1.anguloDeg(a2));
+            Console.WriteLine(a2.anguloRad(a1));
+            Console.WriteLine(i.anguloDeg(j));
+
+            Console.WriteLine("Projeções: ");
+            Console.WriteLine(a1.projecaoEm(a2));
+            Console.WriteLine(a2.projecaoEm(a1));
+            Console.WriteLine(a1.projecaoEm(i));
+            Console.WriteLine(i.projecaoEm(j));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ caVector2D/caVector2D/Vetor2D.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caVector2D
+{
+    class Vetor2D
+    {
+        //Atributos
+        //-x
+        private double x;
+        //-y
+        private double y;
+        //Metodos
+        //-Construtores
+        public Vetor2D()
+        {
+            x = .0;
+            y = .0;
+        }
+        public Vetor2D(double _x, double _y)
+        {
+            x = _x;
+            y = _y;
+        }
+        //--Getters
+        public double getX()
+        {
+            return x;
+        }
+        public double getY()
+        {
+            return y;
+        }
+        //--Setters
+        public void setX(double _x)
+        {
+            x = _x;
+        }
+        public void setY(double _y)
+        {
+            y = _y;
+        }
+        //---Imprimir Vetor
+        public void print()
+        {
+            Console.WriteLine("(" + x + ", " + y + ")");
+        }
+        //---Produto Escalar
+        public double escalar(Vetor2D _b)
+        {
+            return (x * _b.getX() + y * _b.getY());
+        }
+        //---Módulo Do Vetor
+        public double modulo()
+        {
+            return (Math.Sqrt((x * x + y * y)));
+        }
+        //---Angulo Entre Vetores
+        public double anguloRad(Vetor2D _b)
+        {
+            return (Math.Acos(this.escalar(_b) / (this.modulo() * _b.modulo())));
+        }
+        public double anguloDeg(Vetor2D _b)
+        {
+            return radDeg(this.anguloRad(_b));
+        }
+        //----Radianos Para Graus
+        private double radDeg(double _rad)
+        {
+            return (_rad * (180.0 / Math.PI));
+        }
+        //---Projeção De Um Vetor Em Outro
+       public double projecaoEm(Vetor2D _b)
+        {
+            return this.escalar(_b.divisaoPor(_b.modulo()));
+        }
+        //----Quociente Entre Vetor E Escalar
+        private Vetor2D divisaoPor(double _r)
+        {
+            return new Vetor2D(x/_r, y/_r);
+        }
+    } //Fim Da Classe
+}

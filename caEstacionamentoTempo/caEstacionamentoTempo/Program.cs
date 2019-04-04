834e7b7c812724fb8397b959aa46c9afc771dfef
Gustavo Alves Pacheco <42452998+Gap1512@users.noreply.github.com>
2019-04-04 16:13:52 -0300

    Update Program.cs

--- caEstacionamentoTempo/caEstacionamentoTempo/Program.cs
+++ caEstacionamentoTempo/caEstacionamentoTempo/Program.cs
@@ -22,7 +22,7 @@ namespace caEstacionamentoTempo
             */
 
             Estacionamento estacionamento = new Estacionamento();
-            for (int i = 0; i < 2; i++)
+            for (int i = 0; i < 5; i++)
             {
                 estacionamento.novaEntrada();
             }

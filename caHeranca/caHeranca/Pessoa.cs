19ea3073547b23e3b2d910e1aac774c060d16f17
Gustavo Alves Pacheco <42452998+Gap1512@users.noreply.github.com>
2019-04-24 17:19:06 -0300

    Add files via upload

---
+++ caHeranca/caHeranca/Pessoa.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caHeranca
+{
+    class Pessoa
+    {
+        protected String nome;
+        protected int idade;
+
+        public Pessoa()
+        {
+            nome = "";
+            idade = 0;
+        }
+
+        public Pessoa(String _nome, int _idade)
+        {
+            nome = _nome;
+            idade = _idade;
+        }
+
+        public String getNome()
+        {
+            return nome;
+        }
+
+        public int getIdade()
+        {
+            return idade;
+        }
+
+        public void setNome(String _nome)
+        {
+            nome = _nome;
+        }
+
+        public void setIdade(int _idade)
+        {
+            idade = _idade;
+        }
+    }
+}
---
+++ caHeranca/caHeranca/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caHeranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno A1 = new Aluno();
+            Aluno A2 = new Aluno("Gustavo Alves Pacheco", 20, "Engenharia de Computação");
+
+            A1.imprimeDados();
+            A2.imprimeDados();
+
+            Console.ReadLine();
+        }
+    }
+}

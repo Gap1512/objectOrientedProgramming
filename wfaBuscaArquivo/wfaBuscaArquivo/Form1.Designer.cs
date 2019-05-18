265f5c68adef5b48e722910caa4738e6f4aa962b
Gustavo Alves Pacheco <42452998+Gap1512@users.noreply.github.com>
2019-05-18 12:19:33 -0300

    Add files via upload

---
+++ wfaBuscaArquivo/wfaBuscaArquivo/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace wfaBuscaArquivo
+{
+    partial class formMain
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbInfo = new System.Windows.Forms.Label();
+            this.tbDirEntrada = new System.Windows.Forms.TextBox();
+            this.tbRWArquivo = new System.Windows.Forms.TextBox();
+            this.btSalvarArquivo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbInfo
+            // 
+            this.lbInfo.AutoSize = true;
+            this.lbInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbInfo.Location = new System.Drawing.Point(12, 44);
+            this.lbInfo.Name = "lbInfo";
+            this.lbInfo.Size = new System.Drawing.Size(173, 17);
+            this.lbInfo.TabIndex = 0;
+            this.lbInfo.Text = "Entre arquivo ou diretório:";
+            // 
+            // tbDirEntrada
+            // 
+            this.tbDirEntrada.Location = new System.Drawing.Point(12, 94);
+            this.tbDirEntrada.Name = "tbDirEntrada";
+            this.tbDirEntrada.Size = new System.Drawing.Size(362, 20);
+            this.tbDirEntrada.TabIndex = 1;
+            this.tbDirEntrada.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbDirEntrada_KeyDown);
+            // 
+            // tbRWArquivo
+            // 
+            this.tbRWArquivo.Location = new System.Drawing.Point(12, 151);
+            this.tbRWArquivo.Multiline = true;
+            this.tbRWArquivo.Name = "tbRWArquivo";
+            this.tbRWArquivo.Size = new System.Drawing.Size(362, 351);
+            this.tbRWArquivo.TabIndex = 2;
+            // 
+            // btSalvarArquivo
+            // 
+            this.btSalvarArquivo.Location = new System.Drawing.Point(396, 94);
+            this.btSalvarArquivo.Name = "btSalvarArquivo";
+            this.btSalvarArquivo.Size = new System.Drawing.Size(75, 408);
+            this.btSalvarArquivo.TabIndex = 4;
+            this.btSalvarArquivo.Text = "Gravar No Arquivo";
+            this.btSalvarArquivo.UseVisualStyleBackColor = true;
+            this.btSalvarArquivo.Click += new System.EventHandler(this.btSalvarArquivo_Click);
+            // 
+            // formMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 515);
+            this.Controls.Add(this.btSalvarArquivo);
+            this.Controls.Add(this.tbRWArquivo);
+            this.Controls.Add(this.tbDirEntrada);
+            this.Controls.Add(this.lbInfo);
+            this.Name = "formMain";
+            this.Text = "Busca de Arquivo";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.formMain_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbInfo;
+        private System.Windows.Forms.TextBox tbDirEntrada;
+        private System.Windows.Forms.TextBox tbRWArquivo;
+        private System.Windows.Forms.Button btSalvarArquivo;
+    }
+}
+
---
+++ wfaBuscaArquivo/wfaBuscaArquivo/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace wfaBuscaArquivo
+{
+    public partial class formMain : Form
+    {
+        public formMain()
+        {
+            InitializeComponent();
+        }
+
+        private string obtemInfoArquivo(string pathname)
+        {
+            string info = "";
+
+            info = pathname + " existe\r\n\r\n";
+            info += "Criado em: " + File.GetCreationTime(pathname) + "\r\n";
+            info += "Modificado em: " + File.GetLastWriteTime(pathname) + "\r\n";
+            info += "Acesso em: " + File.GetLastAccessTime(pathname) + "\r\n\r\n";
+
+            return info;
+        }
+
+        private string listarDiretorios(string pathname)
+        {
+            string info;
+            string[] dirs = Directory.GetDirectories(pathname), files = Directory.GetFiles(pathname);
+
+            info = "Sub-Pastas: \r\n";
+            for(int i = 0; i < dirs.Length; i++)
+            {
+                info += dirs[i] + "\r\n";
+            }
+            info += "\r\nArquivos: \r\n";
+            for (int i = 0; i < files.Length; i++)
+            {
+                info += files[i] + "\r\n";
+            }
+            info += "\r\n";
+            return info;
+        }
+
+        private void analyzeInput(string pathname)
+        {
+            if (File.Exists(pathname))
+            {
+                StreamReader stream = new StreamReader(pathname);
+                try
+                {
+                    tbRWArquivo.Text = obtemInfoArquivo(pathname);
+                    tbRWArquivo.Text += stream.ReadToEnd();
+                }
+                catch (IOException IOException)
+                {
+                    MessageBox.Show(
+                        IOException.ToString(),
+                        "Erro Inesperado",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    stream.Close();
+                }
+            }
+            else if (Directory.Exists(pathname))
+            {
+                tbRWArquivo.Text = obtemInfoArquivo(pathname);
+                tbRWArquivo.Text += listarDiretorios(pathname);
+            }
+            else
+            {
+                MessageBox.Show(
+                    "Caminho não encontrado",
+                    "Erro de Arquivo",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        private void tbDirEntrada_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                analyzeInput(tbDirEntrada.Text);
+            }
+        }
+
+        private void salvaArquivo()
+        {
+            string caminho = @"../information.txt";
+            StreamWriter stream = new StreamWriter(caminho);
+            try
+            {
+                if (!(File.Exists(caminho)))
+                {
+                    File.CreateText(caminho);
+                }
+                stream.WriteLine(tbRWArquivo.Text);
+                MessageBox.Show(
+                    "Arquivo salvo em \"./wfaBuscaArquivo/wfaBuscaArquivo/bin/information.txt\"",
+                    "Gravado com sucesso!",
+                    MessageBoxButtons.OK);
+            }
+            catch (IOException IOException)
+            {
+                MessageBox.Show(
+                    IOException.ToString(),
+                    "Erro Inesperado",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+            finally
+            {
+                stream.Close();
+            }
+
+        }
+
+        private void btSalvarArquivo_Click(object sender, EventArgs e)
+        {
+            salvaArquivo();
+        }
+
+        private void formMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult dialogResult =
+            MessageBox.Show(
+                "Deseja Salvar As Alterações Antes De Sair?",
+                "Atenção",
+                MessageBoxButtons.YesNoCancel,
+                MessageBoxIcon.Exclamation);
+
+            if (dialogResult == DialogResult.Yes)
+            {
+                salvaArquivo();
+                e.Cancel = false;
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                e.Cancel = false;
+            }
+            else if (dialogResult == DialogResult.Cancel)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
